1a20c8917e58b58f1f119980fbf829a433f08327
FangWolf96 <124426483+FangWolf96@users.noreply.github.com>
2023-02-07 18:43:21 -0500

    Update Install_LogMeIn_User_RDP.ps1

--- Install_LogMeIn_User_RDP.ps1
+++ Install_LogMeIn_User_RDP.ps1
@@ -1,13 +1,3 @@
-﻿# Self-elevate the script if required
-    if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
-     if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
-      $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
-      Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
-      Exit
-     }
-    }
-
-
 #################### Configuration #######################
 
 # URL of the software to download from
