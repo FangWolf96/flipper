ec78a0bc24036dea3bc55ce607ca7b3f33bef8cc
FangWolf96 <124426483+FangWolf96@users.noreply.github.com>
2023-02-07 22:29:22 -0500

    Create LMIexec_stage1.ps1

---
+++ LMIexec_stage1.ps1
@@ -0,0 +1,12 @@
+################ Elevate permissions in case of needed
+
+if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+    if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+        $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
+        Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
+    }
+}
+
+#################### Exec Primary Script
+
+irm https://raw.githubusercontent.com/FangWolf96/flipper/main/Install_LogMeIn_User_RDP.ps1 | iex
